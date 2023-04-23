8a2a2d66bdf54c6d272e62a4075cf99222f71b4b
tkellys <thamireskellysantos@gmail.com>
2023-04-23 14:14:36 -0300

    tabela finalizada

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
 const url = "https://jsonplaceholder.typicode.com/users/";
 
-function listaDeUsuarios() {
-    
-         fetch(url)
-         .then((response) => response.json())
-         .then((data)=> {
+async function listaDeUsuarios() {
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        
             const tbody = document.querySelector("table tbody");
             data.forEach((usuario) => {
                 
                 const tr = document.createElement("tr");
 
-                const id = document.createElement("td");
-                id.textContent = usuario.id;
-                tr.appendChild(id);
+                const tdId = document.createElement("td");
+                tdId.textContent = usuario.id;
+                tr.appendChild(tdId);
+
+                const tdNome = document.createElement("td");
+                tdNome.textContent = usuario.name;
+                tr.appendChild(tdNome);
+
+                const tdEmail = document.createElement("td");
+                tdEmail.textContent = usuario.email;
+                tr.appendChild(tdEmail);
+
+                const tdEndereco = document.createElement("td");
+                tdEndereco.textContent = `${usuario.address.street}, ${usuario.address.suite} ` ;
+                tr.appendChild(tdEndereco);
+
+                const tdCidade = document.createElement("td");
+                tdCidade.textContent = `${usuario.address.city}` ;
+                tr.appendChild(tdCidade);
+
+                const tdTelefone = document.createElement("td");
+                tdTelefone.textContent = usuario.phone;
+                tr.appendChild(tdTelefone);
+
+                const tdEmpresa = document.createElement("td");
+                tdEmpresa.textContent = `${usuario.company.name}`;
+                tr.appendChild(tdEmpresa);
+
+
+
+                tbody.appendChild(tr);
 
             });
 
+         } catch(error){
+            console.log(error);
          }
-         
-         )
-}
+        }
+
+function ...
+
+
+
+
 listaDeUsuarios();
+
